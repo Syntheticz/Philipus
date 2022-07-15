8539c93710f474ee0273f70e48bf8da6bd8d5ca1
Syntheticz <guiangphilip08@gmail.com>
2022-07-16 02:20:34 +0800

    Edited Styles, Stored the color values into variables.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,2 @@
 gsap.from('#content',{opacity: -1, ease: Power4.easeOut, duration: 1,y: -100, delay: .3})
-gsap.from('.con img',{opacity: 0, ease: Expo.easeOut, x: 100, duration: 3})
+gsap.from('.dp_container img',{opacity: 0, ease: Expo.easeOut, x: 100, duration: 3})
